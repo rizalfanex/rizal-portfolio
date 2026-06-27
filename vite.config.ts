e20d6b87ac91745e951972f9b328345445312590
Mochamad Rizal Fauzan <rizalfauzan2002@gmail.com>
2026-06-27 15:58:11 +0800

    fix: remove unused getBasePath function

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,5 @@
-﻿import { defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-function getBasePath() {
-  const repository = process.env.GITHUB_REPOSITORY;
-
-  if (!repository) {
-    return "/";
-  }
-
-  const repoName = repository.split("/")[1];
-
-  if (!repoName) {
-    return "/";
-  }
-
-  return repoName.endsWith(".github.io") ? "/" : `/${repoName}/`;
-}
 
 export default defineConfig({
   plugins: [react()],
